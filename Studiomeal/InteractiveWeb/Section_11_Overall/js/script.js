f6a262adf6391152c0c6d8adfad2fd357e06a5b9
art11010 <49858735+art11010@users.noreply.github.com>
2020-06-26 14:52:10 +0900

    web

--- Studiomeal/InteractiveWeb/Section_11_Overall/js/script.js
+++ Studiomeal/InteractiveWeb/Section_11_Overall/js/script.js
@@ -1,25 +1,13 @@
 (function(){
-	const bodyElem = document.querySelector('body');
+	const stageElem = document.querySelector('.stage');
 	const houseElem = document.querySelector('.house');
 	const barElem = document.querySelector('.progress-bar');
+	const mousePos = {x:0,y:0};
 	let maxScrollValue;
 
 	function resizeHendler(){
 		maxScrollValue = document.body.offsetHeight - window.innerHeight;
 	}
-
-	// 내가 만든 Bar
-	// const barElem = document.createElement('div');
-	// barElem.className = 'bar'
-	// barElem.innerHTML = '<span></span>'
-	// bodyElem.appendChild(barElem);
-
-	// function iMadeBar(){
-	// 	let barMove = scrollPer * 100;
-	// 	console.log(barMove);
-	// 	barElem.childNodes[0].style.width = barMove + 'vw';
-	// }
-
 	window.addEventListener('scroll',function(){
 		const scrollPer = pageYOffset / maxScrollValue;
 		let zMove = scrollPer * 980 - 490;
@@ -28,13 +16,13 @@
 
 		// progress bar
 		barElem.style.width = scrollPer * 100 + 'vw';
-
-		// 내가 만든 Bar
-		// iMadeBar();
 	});
 
 	window.addEventListener('mousemove',function(e){
-		console.log(e.clientX,e.clientY);
+		mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+		mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+		// console.log(mousePos);
+		stageElem.style.transform = 'rotateX(' + mousePos.y*5 + 'deg) rotateY( ' + mousePos.x*5 + 'deg)'
 	});
 
 	window.addEventListener('resize',resizeHendler);
