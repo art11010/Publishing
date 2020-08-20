d0d31b32d828fa336cad38262bd0ac61440174a1
art11010 <49858735+art11010@users.noreply.github.com>
2020-08-20 17:55:35 +0900

    Update character.js

--- Studiomeal/InteractiveWeb/Section_11_Overall/js/character.js
+++ Studiomeal/InteractiveWeb/Section_11_Overall/js/character.js
@@ -30,23 +30,37 @@ function Character(info){   // 생성자니까 함수명 대문자로 시작
     this.mainElem.style.left = info.xPos + '%';
     this.init();
     this.scrollState = false;
+    // 바로 이전(마지막) 스크롤 위치
+    this.lastScrollTop = 0;
 }
 Character.prototype = {
     constructor : Character,
     init : function(){
         const that = this;
         window.addEventListener('scroll',function(){
-            // clearTimeout(that.scrollState);
+            clearTimeout(that.scrollState);
             if(!that.scrollState){
                 that.mainElem.classList.add('running');
-                // console.log('running 클래스 붙음');
+                console.log('running 클래스 붙음');
             }
             that.scrollState = setTimeout(function(){
                 that.scrollState = false;
                 that.mainElem.classList.remove('running');
                 // console.log('running 클래스 제거');
             },500);
-            console.log(that.scrollState)
-        })
+            // console.log('lastScrollTop : ' + that.lastScrollTop);
+            // console.log('pageYOffset : ' + pageYOffset);
+
+            // 이전 스크롤 위치와 현재 스크롤 위치를 비교
+            if(that.lastScrollTop>pageYOffset){
+                // 이전 스크롤 위치가 크다면 : 스크롤 올림
+                that.mainElem.setAttribute('data-diraction','backward');
+            }else{
+                // 현재 스크롤 위치가 크다면 : 스크롤 내림
+                that.mainElem.setAttribute('data-diraction','forward');
+            }
+            that.lastScrollTop = pageYOffset;
+        });
+        window.addEventListener('keydown',function(e){});
     }
 }
